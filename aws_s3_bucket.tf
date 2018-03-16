99fb28c46418a5dad6a22c7b4a4672d893308821
ujway <satoyu313@gmail.com>
2018-03-16 20:28:13 +0900

    remove s3 due to scope out of aws-ecs

--- aws_s3_bucket.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket" {
-  bucket = "${var.app_base_name}_bucket"
-  acl    = "private"
-  tags {
-    Name        = "${var.app_base_name}"
-    Environment = "Dev"
-  }
-}
